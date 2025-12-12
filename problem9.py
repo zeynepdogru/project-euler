335c303369eeceec3bf9a92f81c3c195ac9474fe
zeynepdogru <zynp11dgru@gmail.com>
2025-12-13 00:28:39 +0300

    problem9 solved.

---
+++ problem9.py
@@ -0,0 +1,8 @@
+# Find the Pythagorean triplet for which a + b + c = 1000
+for a in range (1, 333):
+     for b in range(a+1, (1000 - a)//2 ):
+      c = 1000 - a - b
+      if (a**2 + b**2 == c**2):
+        print(a,b,c,a*b*c)
+    
+    
